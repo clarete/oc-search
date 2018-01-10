09b2656ab97f376e68343a82b20b857243e6bb2a
Lincoln de Sousa <lincoln@comum.org>
2018-01-09 22:42:09 -0500

    Root Endpoint (/) will now map to static directory (dist/client)

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 // @flow
+import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
 import * as controllers from './controllers';
 
 const app = express();
 app.use(morgan('combined'));
-app.get('/', controllers.index);
+app.use('/', express.static(path.join(__dirname, '..', 'client')));
 app.get('/search', controllers.search);
 
 export default app;
--- server/controllers.js
+++ server/controllers.js
@@ -2,10 +2,6 @@
 import type { $Request, $Response } from 'express';
 import { doSearch } from './search';
 
-export async function index(req: $Request, res: $Response): mixed {
-  res.sendStatus(200);
-}
-
 export async function search(req: $Request, res: $Response): mixed {
   res.status(200).json(await doSearch(req.query.q));
 }
