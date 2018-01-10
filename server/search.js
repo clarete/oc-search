d3c41ee198eceb694142e4a3ece6fbe5a1c832c5
Lincoln de Sousa <lincoln@comum.org>
2018-01-09 22:41:04 -0500

    Query the database when /search?q=QUERY is requested

--- server/search.js
+++ server/search.js
@@ -1,4 +1,15 @@
 // @flow
-export async function doSearch(q: string): Object {
-  return { q };
+import knex from './models';
+
+const lowerWhere = (field) => `LOWER(${field}) LIKE ?`;
+
+export async function doSearch(q: string): Promise<Object> {
+  const value = [`%${q.toLowerCase()}%`];
+  const all = await knex('collectives')
+    .whereNull('deletedAt')
+    .andWhere((qb) =>
+      qb.whereRaw(lowerWhere('name'), value)
+        .orWhereRaw(lowerWhere('description'), value)
+        .orWhereRaw(lowerWhere('longDescription'), value));
+  return all;
 }
